83eea70dd72a10f77b73b78dd0a582eed5a14532
sofiamilanes <sofiamilanes27@gmail.com>
2022-05-02 16:16:41 -0700

    working sqlAl

--- model.py
+++ model.py
@@ -1,8 +1,18 @@
-from email.policy import default
-from xmlrpc.client import boolean
+# from email.policy import default
+# from xmlrpc.client import boolean
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def connect_to_db(app, db_name):
+    """Connect to database."""
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql:///{db_name}"
+    app.config["SQLALCHEMY_ECHO"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = app
+    db.init_app(app)
 
 
 class User(db.Model):
@@ -13,10 +23,11 @@ class User(db.Model):
     lname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), nullable=False)
 
-    #backref to ratings
+    rating = db.relationship("Ratings", back_populates="user") #!user or users 
+    fav_place = db.relationship("User_fav_places", back_populates="user")
 
-class Fav_places(db.Model):
-    __tablename__ = "fav_places"
+class Places(db.Model):
+    __tablename__ = "places"
 
     place_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
@@ -24,7 +35,8 @@ class Fav_places(db.Model):
     zip_code = db.Column(db.Integer, nullable=False)
     address = db.Column(db.String(50), nullable=False)
 
-    #backref to ratings
+    rating = db.relationship("Ratings", back_populates="place") #! places or place
+    fav_place = db.relationship("User_fav_places", back_populates="place")
 
 class Ratings(db.Model):
 
@@ -32,26 +44,37 @@ class Ratings(db.Model):
 
     rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     score = db.Column(db.Integer, nullable=False)
-    favorite_place_id = db.Column(db.Integer, db.ForeignKey("fav_places.place_id"), nullable=False)
+    favorite_place_id = db.Column(db.Integer, db.ForeignKey("places.place_id"), nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey("users.user_id"), nullable=False)
     comment = db.Column(db.Text, nullable=True)
    
 
-    place = db.relationship("Fav_places", backref="ratings")
-    user = db.relationship("User", backref="ratings")
+    place = db.relationship("Places", back_populates="rating")
+    user = db.relationship("User", back_populates="rating")
 
 class User_fav_places(db.Model):
 
     __tablename__ = "user_fav_places"
 
-    favorite_place_id = db.Column(db.Integer, db.ForeignKey("fav_places.place_id"), nullable=False)
+    fav_places_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    favorite_place_id = db.Column(db.Integer, db.ForeignKey("places.place_id"), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
-    likes = db.Column(db.boolean, default = True, nullable = False) #*make sure this is how you write it
+    likes = db.Column(db.Boolean, default = True, nullable = False) #*make sure this is how you write it
+
+    place = db.relationship("Places", back_populates="fav_place") #!
+    user = db.relationship("User", back_populates="fav_place") #!
+
 
 class Type_of_place(db.Model):
 
     __tablename__ = "type_of_place"
 
+    type_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     type = db.Column(db.String(70), nullable=False)
-    favorite_place_id = db.Column(db.Integer, db.ForeignKey("fav_places.place_id"), nullable=False)
+    favorite_place_id = db.Column(db.Integer, db.ForeignKey("places.place_id"), nullable=False)
     
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app, "users")
