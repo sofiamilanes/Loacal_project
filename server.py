e3912dfd59ddf3dc7848e9a49fe114eb79ef5609
sofiamilanes <sofiamilanes27@gmail.com>
2022-05-04 13:08:58 -0700

    checking if user is in DB working

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 
+import email
 from flask import Flask, render_template, redirect, request, session, flash
 from yelp_search import get_results
 from model import connect_to_db
@@ -22,42 +23,79 @@ def login():
 
 @app.route('/authentication', methods = ['POST'])
 def authentication():
-    """gets the information from log in and makes sure this person is in session"""
+    """gets the information from log in and makes sure this person is in the database"""
     email = request.form['email']
     password = request.form['password']
-    print(password)
-    print(session['password'])
 
-    if email != session['email']: #! need to change this to check on the db not session
-        flash("Incorrect password or email. Please try again")
-        return redirect('/login')
-    if password != session['password']:#! need to change this to check on the db not session
-        flash("Incorrect password or email. Please try again")
-        return redirect('/login')
-    return redirect('/homepage')
+    user = crud.get_user_by_email(email)
+
+    if user:
+        if password == user.password:
+            session["first_name"] = user.fname
+            print(session['first_name'])
+            return redirect('/homepage')
+
+    flash("Incorrect password or email. Please try again")
+    return redirect('/login')
 
 
+    # if email != session['email']: #! need to change this to check on the db not session
+    #     flash("Incorrect password or email. Please try again")
+    #     return redirect('/login')
+    # if password != session['password']:#! need to change this to check on the db not session
+    #     flash("Incorrect password or email. Please try again")
+    #     return redirect('/login')
+    # return redirect('/homepage')
+
+#! need to figure out how to get then name onto the page without session 
 @app.route('/homepage')
 def homepage():
 
  return render_template('homepage.html')
 
+
 @app.route('/registration', methods = ["POST"])
 def registration():
 
-    if request.form['conf-password'] != request.form['password']:
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email= request.form['email']
+    password = request.form['password']
+
+
+    if request.form['conf-password'] != password:
         flash("passwords did not match please try again")
         return redirect('/')
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash("Email already exist, please try to log in")
+        return redirect('/')
+
     else:
-        session['first_name'] = request.form['first_name']
-        session['last_name'] = request.form['last_name']
-        session['email'] = request.form['email']
-        session['password'] = request.form['password']
+        crud.create_user(fname = first_name,
+                lname = last_name,
+                email= email,
+                password=password )
+        flash("Account created! Please log in")
 
-        crud.create_user(fname = session['first_name'], lname = session['last_name'], email=session['email'], password=session['password'] )
+    return redirect('/login')
 
 
-    return redirect('/homepage')
+
+    # else:
+    #     session['first_name'] = request.form['first_name']
+    #     session['last_name'] = request.form['last_name']
+    #     session['email'] = request.form['email']
+    #     session['password'] = request.form['password']
+
+    #     crud.create_user(fname = session['first_name'],
+    #                     lname = session['last_name'],
+    #                     email=session['email'],
+    #                     password=session['password'] )
+
+
+    # return redirect('/homepage')
 
 
 @app.route('/results', methods = ["POST"])
